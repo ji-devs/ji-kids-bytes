964a53a011b7dc076b562c6d09395ca7f3870733
David Komer <david.komer@gmail.com>
2020-03-25 20:41:58 +0200

    make color theme

--- typescript/components.ts
+++ typescript/components.ts
@@ -2,3 +2,4 @@ import "@components/pages/topic/topic";
 import "@components/pages/not-found/not-found";
 import "@components/pages/loading/loading";
 import "@components/pages/home/home";
+import "@components/footer/footer";
